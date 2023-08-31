4c60a5f51cb8bd9764d8ad65c19df40ed191e106
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-08-31 18:56:12 +0900

    Save survey results in TSV format

--- src/main/java/com/github/iunius118/oresurvey/client/OreSurveyClient.java
+++ src/main/java/com/github/iunius118/oresurvey/client/OreSurveyClient.java
@@ -5,20 +5,34 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringJoiner;
 
 public class OreSurveyClient implements ClientModInitializer {
     public static final String MOD_ID = "oresurvey";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     private static OreSurveyor oreSurveyor;
 
@@ -38,15 +52,16 @@ public class OreSurveyClient implements ClientModInitializer {
                 }
 
                 surveyOres(oreSurveyor, client);
-                System.out.println("Survey ores: #" + oreSurveyor.getSurveyCount());
             }
 
             while (keyResult.consumeClick()) {
-                if (oreSurveyor != null) {
-                    printResult(oreSurveyor);
+                if (oreSurveyor == null) {
+                    oreSurveyor = OreSurveyor.ofDefault();
+                    surveyOres(oreSurveyor, client);
                 }
 
-                oreSurveyor = OreSurveyor.ofDefault();
+                saveResult(oreSurveyor);
+                oreSurveyor = null;
             }
         });
     }
@@ -63,10 +78,11 @@ public class OreSurveyClient implements ClientModInitializer {
 
         BlockPos pos = player.getOnPos();
 
-        try (var level = player.level()) {
-            surveyOres(oreSurveyor, level, pos);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        try {
+            surveyOres(oreSurveyor, player.level(), pos);
+            client.player.displayClientMessage(createChatMessage(Component.literal("Survey ores: #" + oreSurveyor.getSurveyCount())), false);
+        } catch (Exception e) {
+            LOGGER.warn("surveyOres:%n", e);
         }
     }
 
@@ -74,28 +90,64 @@ public class OreSurveyClient implements ClientModInitializer {
         oreSurveyor.surveyOres(level, pos);
     }
 
-    private void printResult(OreSurveyor oreSurveyor) {
-        OreSurveyor.SurveyResult result = oreSurveyor.getResult();
+    private void  saveResult(OreSurveyor oreSurveyor) {
+        Minecraft client = Minecraft.getInstance();
+        File gameDirectory = client.gameDirectory;
+        String filename = getTimestamp() + ".tsv";
 
-        StringJoiner oreIDs = new StringJoiner("\t", "\nlevel\t", "\n");
+        var path = Paths.get(gameDirectory.getPath(), MOD_ID, filename);
 
-        for(Block block : result.ores()) {
-            oreIDs.add(BuiltInRegistries.BLOCK.getKey(block).toString());
+        try {
+            // Create parent dirs
+            Files.createDirectories(path.getParent());
+        } catch (IOException e) {
+            LOGGER.warn("saveResult:%n", e);
         }
 
-        System.out.print(oreIDs);
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path, StandardCharsets.UTF_8);
+             PrintWriter writer = new PrintWriter(bufferedWriter)) {
+            printResult(writer, oreSurveyor);
+            if (client.player != null)
+                client.player.displayClientMessage(createChatMessage(Component.literal("Result saved as " + Paths.get(MOD_ID, filename))), false);
+        } catch (IOException e) {
+            LOGGER.warn("saveResult:%n", e);
+        }
+    }
 
+    private String getTimestamp() {
+        var timestamp = new Timestamp(System.currentTimeMillis());
+        var dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS");
+        return dateFormat.format(timestamp);
+    }
+
+    private void printResult(PrintWriter writer, OreSurveyor oreSurveyor) {
+        OreSurveyor.SurveyResult result = oreSurveyor.getResult();
+
+        // Write header record
+        StringJoiner header = new StringJoiner("\t").add("Layer");
+
+        for(Block block : result.ores()) {
+            header.add(BuiltInRegistries.BLOCK.getKey(block).toString());
+        }
+
+        writer.println(header);
+
+        // Write data records
         Set<Map.Entry<Integer, int[]>> entries = result.data().entrySet();
 
         for(Map.Entry<Integer, int[]> entry : entries) {
-            StringJoiner data = new StringJoiner("\t", "", "\n");
+            StringJoiner data = new StringJoiner("\t");
             data.add(entry.getKey().toString());
 
             for(int value : entry.getValue()) {
                 data.add(String.valueOf(value));
             }
 
-            System.out.print(data);
+            writer.println(data);
         }
+    }
+
+    private Component createChatMessage(MutableComponent message) {
+        return Component.literal("").append(Component.literal("[OreSurvey] ").withStyle(ChatFormatting.YELLOW)).append(message.withStyle(ChatFormatting.RESET));
     }
 }
